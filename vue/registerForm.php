8a1361bd8c3866b1506193d896e1f90f42fd52e4
AnthoLC77 <mishynaro@hotmail.fr>
2023-12-14 16:13:54 +0100

    rajout des attributs patern html

--- vue/registerForm.php
+++ vue/registerForm.php
@@ -19,13 +19,14 @@
         </p>
         <form action="" method="POST">
             <label>Nom</label>
-            <input type="text" name="lastname">
+            <input type="text" name="lastname" required pattern="[A-Za-z0-9]+" >
             <label>Prénom</label>
-            <input type="text" name="firstname">
+            <input type="text" name="firstname" required minlength="2" maxlength="255" pattern="[A-Za-z0-9]+" >
             <label>Email</label>
-            <input type="email" name="email">
+            <input type="email" name="email" required minlength="2" maxlength="255" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$">
             <label>Mot de passe</label>
-            <input type="password" name="password">
+            <input type="password" name="password" required minlength="2" maxlength="255"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters">
             <input type="submit" value="Ajouter" name="button">
         </form>
     </div>
